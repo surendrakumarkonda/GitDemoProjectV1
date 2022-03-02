8123ee4494a9d81448da04e2d731c5e64eecbb98
Guddu <Guddu@LAPTOP-LCS0O6RD>
2022-03-02 11:05:04 +0530

    First commit...

---
+++ src/GitPackage/Test.java
@@ -0,0 +1,21 @@
+package GitPackage;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry="Yes";
+		
+		if(hungry=="yes") {
+			
+			System.out.println("khub khao dabake:");
+		}
+		
+		else {
+			System.out.println("bhukhe maro:");
+		}
+
+	}
+
+}
